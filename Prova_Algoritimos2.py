754167e1e95d555ceb09ecbc5e011bf42875c51f
Gabriel Costa <bastosk8@hotmail.com>
2024-06-12 14:33:58 -0300

    provas

---
+++ Prova_Algoritimos2.py
@@ -0,0 +1,10 @@
+#A partir do que aprendemos até agora:
+#Crie um exemplo de cada uma das principais tipagens de variáveis.
+#Crie uma solicitação de dados ao usuário.
+#Imprima a solicitação anterior na tela com uma mensagem personalizada.
+mensagem = input('Digite sua mensagem: ') #string
+idade = int(input('Digite sua idade: ')) #inteiro
+nome = input ('Digite sue nome: ') 
+altura = float(input('Digite sua altura: ')) #float
+brasileiro = True #bool
+print (f'Sua mensagem é',[mensagem], 'seu nome é',[nome], 'sua idade é',[idade],'anos','sua altura é',[altura])
